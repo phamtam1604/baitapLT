3f13ffceada557adaf2de56431f08c3faab1ae47
LapTrinhHDT <tamk965@gmail.com>
2021-11-04 09:14:00 +0700

    sua

--- src/com/tampv2008110296/kiemtrgiuaki/DanhSachQuanLy.java
+++ src/com/tampv2008110296/kiemtrgiuaki/DanhSachQuanLy.java
@@ -5,29 +5,10 @@
 package com.tampv2008110296.kiemtrgiuaki;
 
 public class DanhSachQuanLy {
-    static ArrayList<Nguoi> list = new ArrayList<Nguoi>();
-    static Scanner nhap = new Scanner(System.in);
-    public static void main(String[] args) {
-        List<String> list1 = new ArrayList<>();
-        for (int i = 1; i <= 2; i++) {
-            // Add element to list
-            list1.add("0" + i);
-        }
-        System.out.print("Init list1 = ");
-        printData(list1);
-  
-        
-        List<String> list2 = new ArrayList<>();
-        list2.addAll(list1);
-        list2.add("0" + 2);
-        System.out.print("After list1.addAll(03): list2 = ");
-        printData(list2);
+    private ArrayList<Nguoi> danhSach = new ArrayList<Nguoi>();
+    public void them(Nguoi nguoi){
+        this.danhSach.add(nguoi);
     }
-    public static void themNguoi(){
-     System.out.println("Nhap them nguoi");
+    public void inDanhSach(){
+        System.out.println(danhSach);
     }
-    public static void inDanhSach(){
-        System.out.println("In danh sach");
-
-    }
-}
--- src/com/tampv2008110296/kiemtrgiuaki/QuanLyTestDrive.java
+++ src/com/tampv2008110296/kiemtrgiuaki/QuanLyTestDrive.java
@@ -6,7 +6,33 @@ package com.tampv2008110296.kiemtrgiuaki;
 
 public class QuanLyTestDrive {
     public static void main(String[] args) {
+        DanhSachQuanLy ds = new DanhSachQuanLy();
+        HocVien sv = new HocVien();
+        sv.setDiemMonHoc1(5);
+        sv.setDiemMonHoc2(6);
+        sv.setHoTen("a");
+        sv.setDiaChi("dia chi o dau");
+        sv.setSoLuongHV(20);
+        ds.them(sv);
+
+        NhanVien nv = new NhanVien();
+        nv.setHeSoLuong(5);
+        nv.setHoTen("B");
+        nv.setDiaChi("dia chi");
+        ds.them(nv);
         
+        KhachHang kh = new KhachHang();
+        kh.setTenCongTy("mb");
+        kh.setTriGiaDonHang(1000);
+        kh.setHoTen(" C");
+        kh.setDiaChi("dia chi");
+        ds.them(kh);
+        ds.inDanhSach();
+
+
+
+
+
     }
     
 }
